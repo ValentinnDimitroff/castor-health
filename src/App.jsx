663f5970288fff4b22dcee4f925013808db58922
Valentin Dimitroff <26602880+ValentinnDimitroff@users.noreply.github.com>
2021-04-16 14:35:35 +0300

    Extract theme from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
 import { ThemeProvider } from 'react-jss'
-import { BrowserRouter as Router } from "react-router-dom";
-import { Layout } from './components';
-import Routes from './Routes';
-
-const theme = {
-	colors: {
-		primary: {
-			light: '#5FAFFF',
-			main: '#3779ED',
-			dark: '#3070CE',
-		},
-		error: {
-			light: '#FF8080',
-			main: '#DC143C',
-			dark: '#BB022E',
-		},
-		success: {
-			light: '#A5FFC6',
-			main: '#2CD06C',
-			dark: '#28BC5F',
-		}
-	},
-	font: {
-		heading: {
-			// TODO
-		}
-	},
-	spacing: (val) => `${val * 8}px`
-}
+import { BrowserRouter as Router } from "react-router-dom"
+import { Layout } from './components'
+import { theme } from './theme'
+import Routes from './Routes'
 
 const App = () => {
 	return (
---
+++ src/theme.js
@@ -0,0 +1,54 @@
+const pUnit = 8
+const regularFont = 400 
+const semiBoldFont = 600 
+
+export const theme = {
+    colors: {
+        primary: {
+            light: '#5FAFFF',
+            main: '#3779ED',
+            dark: '#3070CE',
+        },
+        error: {
+            light: '#FF8080',
+            main: '#DC143C',
+            dark: '#BB022E',
+        },
+        success: {
+            light: '#A5FFC6',
+            main: '#2CD06C',
+            dark: '#28BC5F',
+        }
+    },
+    font: {
+        heading: {
+            fontWeight: regularFont,
+            fontSize: '32px',
+        },
+        title: {
+            fontWeight: semiBoldFont,
+            fontSize: '21px',
+        },
+        subtitle: {
+            fontWeight: semiBoldFont,
+            fontSize: '16px',
+        },
+        body: {
+            fontWeight: regularFont,
+            fontSize: '14px',
+        },
+        label: {
+            fontWeight: semiBoldFont,
+            fontSize: '14px',
+            color: '#3A4963',
+        },
+        body2: {
+            fontWeight: regularFont,
+            fontSize: '13px',
+            color: '#616D82',
+        }
+    },
+    spacing: (val) => `${val * pUnit}px`,
+    spacingY: (val) => `0 ${val * pUnit}px 0 ${val * pUnit}px`,
+    spacingX: (val) => `${val * pUnit}px 0 ${val * pUnit}px 0`,
+}
