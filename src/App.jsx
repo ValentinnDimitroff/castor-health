d9ee57319833c163c3e2ae29779be8a4582ec304
Valentin Dimitroff <26602880+ValentinnDimitroff@users.noreply.github.com>
2021-04-16 10:54:58 +0300

    Refactor scaffolded template

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { ThemeProvider } from 'theming';
+import { Layout } from './components';
+
+const theme = {
+	colors: {
+		primary: {
+			light: '#5FAFFF',
+			main: '#3779ED',
+			dark: '#3070CE',
+		},
+		error: {
+			light: '#FF8080',
+			main: '#DC143C',
+			dark: '#BB022E',
+		},
+		success: {
+			light: '#A5FFC6',
+			main: '#2CD06C',
+			dark: '#28BC5F',
+		}
+	},
+	font: {
+		heading: {
+			// TODO
+		}
+	}
+}
+
+const App = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<Layout>
+				Routing
+			</Layout>
+		</ThemeProvider>
+	);
+}
+
+export default App;
